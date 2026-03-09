7440c4ab2693451b4e7e980f2ecf4f716aff4cad
Pedro-3L <lando133@gmail.com>
2026-03-09 00:09:42 -0600

    Adicion ejercicio 14

---
+++ Ejercicio 14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double Dividir(double a, double b)
+			{
+				return a / b;
+			}
+			Console.WriteLine("Ingrese un numero");
+			double num1 = double.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese otro numero");
+			double num2 = double.Parse(Console.ReadLine());
+			double resultado = Dividir(num1, num2);
+			Console.WriteLine("La division es : " + resultado);
+		}
+	}
+}
---
+++ Ejercicio 14/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Ejercicio 14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ejercicio 14")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("678b017f-c30a-4107-87be-ded6eaad8f63")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
