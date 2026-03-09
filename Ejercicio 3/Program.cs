b3c3d0ada2a7772a92868fcd4d6c45ec127c8462
Pedro-3L <lando133@gmail.com>
2026-03-08 23:14:36 -0600

    Adicion ejercicio 3

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			void MostrarMensaje()
+			{
+				Console.WriteLine("Programación en C#");
+			}
+			MostrarMensaje();
+		}
+	}
+}
