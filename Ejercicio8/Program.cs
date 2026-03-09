dc9ea1f820bd1d5c615a73c4352d77f121f96fca
Pedro-3L <lando133@gmail.com>
2026-03-08 23:47:25 -0600

    adicion ejercicio 8

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int MostrarCuadrado(int n)
+			{
+				return n * n;
+			}
+			Console.WriteLine("Ingrese un numero");
+			int numero = int.Parse(Console.ReadLine());
+			int resultado = MostrarCuadrado(numero);
+			Console.WriteLine("El cuadrado de " + numero + " es: " + resultado);
+		}
+	}
+}
