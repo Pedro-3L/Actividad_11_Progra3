9d1682fa314ea7c59cbde3cdb78bacdc02ff33e6
Pedro-3L <lando133@gmail.com>
2026-03-08 23:10:38 -0600

    Adicion ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			void MostrarNombre()
+			{
+				Console.WriteLine("Alejandro");
+			}
+			MostrarNombre();
+		}
+	}
+}
