ccaf3bf0c08f05a105b9e8976122893871713eaf
Pedro-3L <lando133@gmail.com>
2026-03-08 23:39:04 -0600

    Adicion Ejercicio 6

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			void Saludar(string nombre)
+			{
+				Console.WriteLine("Hola " + nombre);
+			}
+
+			Console.WriteLine("Ingrese un nombre:");
+			 string n = Console.ReadLine();
+			Saludar(n);
+
+		}
+	}
+}
