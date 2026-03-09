10565d5677d09612d8e89d4c6833d784c9d741df
Pedro-3L <lando133@gmail.com>
2026-03-08 23:07:01 -0600

    Adicion ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			void Saludar()
+			{
+				Console.WriteLine("Hola, bienvenido");
+			}
+			Saludar();
+		}
+	}
+}
