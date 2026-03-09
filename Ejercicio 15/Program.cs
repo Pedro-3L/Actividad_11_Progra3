264d67a23bfb8adc9e9ae43310c00a532d56bc23
Pedro-3L <lando133@gmail.com>
2026-03-09 00:18:47 -0600

    Adicion ejercicio 15

---
+++ Ejercicio 15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio_15
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			bool EsPar(int n)
+			{
+				return n % 2 == 0;
+			}
+
+
+			Console.WriteLine("Ingrese un numero:");
+			int numero = int.Parse(Console.ReadLine());
+
+			bool resultado = EsPar(numero);
+
+			Console.WriteLine("¿Es par?: " + resultado);
+
+		}
+	}
+}
