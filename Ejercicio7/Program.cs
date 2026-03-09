9f426af5e9bcf0394cfb78b26e13d5a5b981afd0
Pedro-3L <lando133@gmail.com>
2026-03-08 23:43:35 -0600

    Adicion ejercicio 7

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int MostrarDoble(int n)
+			{
+				return 2 * n;
+			}
+			Console.WriteLine("Ingrese un numero");
+			int numero = int.Parse(Console.ReadLine());
+			int resultado = MostrarDoble(numero);
+			Console.WriteLine("El doble de "+ numero + " es: " + resultado);
+		}
+	}
+}
