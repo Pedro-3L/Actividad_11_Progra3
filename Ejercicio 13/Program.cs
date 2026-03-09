b0e095fd3969adefd2e1e292b2bb14179e960a3b
Pedro-3L <lando133@gmail.com>
2026-03-09 00:06:37 -0600

    Adicion Ejercicio 13

---
+++ Ejercicio 13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_13
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int Multiplicar(int a, int b)
+			{
+				return a * b;
+			}
+			Console.WriteLine("Ingrese un numero");
+			int num1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese otro numero");
+			int num2 = int.Parse(Console.ReadLine());
+			int resultado = Multiplicar(num1, num2);
+			Console.WriteLine("Resultado: " + resultado);
+		}
+	}
+}
