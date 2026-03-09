5237eb9d7213884e7f89ccf6df281974223eaff2
Pedro-3L <lando133@gmail.com>
2026-03-09 00:02:01 -0600

    Adicion ejercicio 11

---
+++ Ejercicio 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int Sumar(int a, int b)
+			{
+				return a +b;
+			}
+			Console.WriteLine("Ingrese un numero");
+			int num1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese otro numero");
+			int num2 = int.Parse(Console.ReadLine());
+			int resultado = Sumar(num1, num2);
+			Console.WriteLine("Resultado: " + resultado);
+		}
+	}
+}
