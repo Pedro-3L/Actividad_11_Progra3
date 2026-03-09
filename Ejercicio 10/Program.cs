3bb87a25b895f99615fb04811dd917f455cedb50
Pedro-3L <lando133@gmail.com>
2026-03-08 23:57:57 -0600

    Adicion ejercico 10

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio_10
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			void ImprimirRepetido(string texto, int veces)
+			{
+				for (int i = 0; i < veces; i++)
+				{
+					Console.WriteLine(texto);
+				}
+			}
+
+			Console.WriteLine("Ingrese un texto:");
+			string t = Console.ReadLine();
+
+			Console.WriteLine("Ingrese cuantas veces quiere imprimirlo:");
+			int v = int.Parse(Console.ReadLine());
+
+			ImprimirRepetido(t, v);
+
+		}
+	}
+}
