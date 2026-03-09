e6d93de6960e3dc3e5c3faef28a402316a31cc2e
Pedro-3L <lando133@gmail.com>
2026-03-08 23:29:41 -0600

    Adicion ejercicio 5

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			void MostrarMenu()
+			{
+				int opcion;
+				Console.WriteLine("1 - Opcion 1");
+				Console.WriteLine("2 - Opcion 2");
+				Console.WriteLine("3 - Salir");
+				opcion = int.Parse(Console.ReadLine());
+				switch (opcion)
+				{
+					case 1:
+						Console.WriteLine("Seleccionada opcion 1");
+						break;
+					case 2:
+						Console.WriteLine("Seleccionada opcion 2");
+						break;
+					case 3:
+						Console.WriteLine("Saliendo");
+						break;
+					default:
+						Console.WriteLine("Opcion invalida");
+						break;
+				}
+			}
+			MostrarMenu();
+		}
+	}
+}
