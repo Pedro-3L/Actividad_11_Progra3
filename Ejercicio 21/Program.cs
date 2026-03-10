d9a7a47004bbab569dd6d476236286be5eefefdf
Pedro-3L <lando133@gmail.com>
2026-03-09 21:13:54 -0600

    Adicion ejercicio 21 y 22

---
+++ Ejercicio 21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio_21
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int ContarVocales(string texto)
+			{
+				int contador = 0;
+
+				foreach (char c in texto)
+				{
+					if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+					{
+						contador++;
+					}
+				}
+
+				return contador;
+			}
+
+			Console.WriteLine("Ingrese un texto: ");
+			string t = Console.ReadLine();
+
+			int cantidad = ContarVocales(t);
+			Console.WriteLine("Cantidad de vocales: " + cantidad);
+
+
+		}
+	}
+}
---
+++ Ejercicio 22/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_22
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string Repetir(string texto, int veces)
+			{
+				string resultado = "";
+
+				for (int i = 0; i < veces; i++)
+				{
+					resultado += texto + "\n";
+				}
+
+				return resultado;
+			}
+
+			Console.WriteLine("Ingrese un texto:");
+			string t = Console.ReadLine();
+
+			Console.WriteLine("Ingrese cuántas veces repetirlo:");
+			int v = int.Parse(Console.ReadLine());
+
+			string r = Repetir(t, v);
+
+			Console.WriteLine(r);
+
+
+		}
+	}
+}
