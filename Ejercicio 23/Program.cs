676e7cd641e31fb292ead36e3c052bf8a8633eed
Pedro-3L <lando133@gmail.com>
2026-03-09 22:13:34 -0600

    Adicion ejercicio 23

---
+++ Ejercicio 23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_23
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int SumarDel1AlN( int n)
+			{
+				int resultado = 0;
+
+				for (int i = 1; i <= n; i++)
+				{
+					resultado += i;
+				}
+
+				return resultado;
+			}
+
+			Console.WriteLine("Ingrese un numero:");
+			int numero = int.Parse(Console.ReadLine());
+
+			int r = SumarDel1AlN(numero);
+
+			Console.WriteLine("La suma de todos los numeros del 1 a " + numero + " es: " + r);
+
+
+		}
+	}
+}
