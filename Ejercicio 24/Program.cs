5be4059e5b0baef5933db200cb3795b0e586187e
Pedro-3L <lando133@gmail.com>
2026-03-09 22:21:14 -0600

    Adicion ejercicio 24 y 25

---
+++ Ejercicio 24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_24
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int Factorial(int n)
+			{
+				int resultado = 1;
+
+				for (int i = 1; i <= n; i++)
+				{
+					resultado *= i;
+				}
+
+				return resultado;
+			}
+
+			Console.WriteLine("Ingrese un numero:");
+			int numero = int.Parse(Console.ReadLine());
+
+			int r = Factorial(numero);
+
+			Console.WriteLine("El factorial de " + numero + " es: " + r);
+		}
+	}
+}
---
+++ Ejercicio 25/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ejercicio_25
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			bool EsPrimo(int n)
+			{
+				if (n <= 1)
+				{
+					return false;
+				}
+
+				for (int i = 2; i < n; i++)
+				{
+					if (n % i == 0)
+					{
+						return false;
+					}
+				}
+
+				return true;
+			}
+
+			Console.WriteLine("Ingrese un numero:");
+			int numero = int.Parse(Console.ReadLine());
+
+			bool r = EsPrimo(numero);
+
+			if (r)
+			{
+				Console.WriteLine("El numero es primo");
+			}
+			else
+			{
+				Console.WriteLine("El numero no es primo");
+			}
+
+		}
+	}
+}
